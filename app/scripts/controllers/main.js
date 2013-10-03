61c6c5d7c73dbe660f971b434f0ff2eeaf874bd4
Bing Han, Goh <stiucsib86@hotmail.com>
2013-10-03 23:45:00 +0800

    Initial Commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('seamlessIframeTestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
